af886bb47c53e1f1512faf0809f22c15614c1d51
SodaOnRails <vitorandresaviandev@gmail.com>
2021-02-23 21:41:36 -0300

    Second Chapter Done

--- alura-golang-fundamentos/hello.go
+++ alura-golang-fundamentos/hello.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	//No go quando vc nao atribui valor a uma var ele retorna 0 ou ""
+	var nome string = "Virto"
+	//var idade = 12
+	versao := 1.1 //isso daqui tbm funciona pq ele pega o tipo e ainda coloca o tipo
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	//Golang só aceita se receber apenas operadores que mandam true ou false
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa, FONTRAB")
+	} else {
+		fmt.Println("Não conheço esse comando")
+	}
 }
